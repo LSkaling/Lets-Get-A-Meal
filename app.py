9d2ec3a7fa082f16999b07e59d0103d86824aef0
Lawton Skaling <lskaling@icloud.com>
2024-12-24 15:29:00 -0900

    Initial release

---
+++ app.py
@@ -0,0 +1,123 @@
+from flask import Flask, render_template
+from datetime import datetime, timedelta
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+import os.path
+import google.auth.transport.requests
+import pickle
+import os
+from dotenv import load_dotenv
+import pytz
+import logging
+logging.getLogger('root').setLevel(logging.ERROR)
+
+load_dotenv()
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+def authenticate():
+    creds = None
+    # Token is saved after first authentication
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # Authenticate if no valid credentials
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(google.auth.transport.requests.Request())
+        else:
+            from google_auth_oauthlib.flow import InstalledAppFlow
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the token for later runs
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    return creds
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    tz = pytz.timezone('America/Los_Angeles')
+
+    cards = [
+        {"title": "Card 1", "box1": {"large": "Busy", "small": "Lunch", "color": "#f0f0f0"}, "box2": {"large": "Busy", "small": "Dinner", "color": "#e6e6e6"}},
+        {"title": "Card 2", "box1": {"large": "Busy", "small": "Lunch", "color": "#f0f0f0"}, "box2": {"large": "Busy", "small": "Dinner", "color": "#e6e6e6"}},
+        {"title": "Card 3", "box1": {"large": "Busy", "small": "Lunch", "color": "#f0f0f0"}, "box2": {"large": "Busy", "small": "Dinner", "color": "#e6e6e6"}},
+        {"title": "Card 4", "box1": {"large": "Busy", "small": "Lunch", "color": "#f0f0f0"}, "box2": {"large": "Busy", "small": "Dinner", "color": "#e6e6e6"}},
+        {"title": "Card 5", "box1": {"large": "Busy", "small": "Lunch", "color": "#f0f0f0"}, "box2": {"large": "Busy", "small": "Dinner", "color": "#e6e6e6"}},
+        {"title": "Card 6", "box1": {"large": "Busy", "small": "Lunch", "color": "#f0f0f0"}, "box2": {"large": "Busy", "small": "Dinner", "color": "#e6e6e6"}},
+        {"title": "Card 7", "box1": {"large": "Busy", "small": "Lunch", "color": "#f0f0f0"}, "box2": {"large": "Busy", "small": "Dinner", "color": "#e6e6e6"}},
+    ]
+
+    for index, card in enumerate(cards):
+        today = datetime.now(tz)
+        # convert to start of day
+        today = today.replace(hour=0, minute=0, second=0, microsecond=0)
+        days_until = index
+        date_formatted = today + timedelta(days=days_until)
+        card["title"] = date_formatted.strftime("%A, %b %d")
+
+    creds = authenticate()
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Get current date in tz and set to midnight
+    now = datetime.now(tz).replace(hour=0, minute=0, second=0, microsecond=0)
+
+    # Calculate next week's date at midnight
+    next_week = now + timedelta(days=7)
+
+    # Convert to ISO format with timezone info
+    now_iso = now.isoformat()
+    next_week_iso = next_week.isoformat()
+
+    events_result = service.events().list(
+        calendarId='f3197c716485707a3468b83c415a7200495fb284fc666d8f38753b8fd31c0652@group.calendar.google.com',
+        timeMin=now_iso,
+        timeMax=next_week_iso,
+        singleEvents=True,
+        orderBy='startTime'
+    ).execute()
+
+    events = events_result.get('items', [])
+
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        start = datetime.strptime(start, "%Y-%m-%dT%H:%M:%S%z")
+        start = start.astimezone(tz)
+        
+        end = event['end'].get('dateTime', event['end'].get('date'))
+        end = datetime.strptime(end, "%Y-%m-%dT%H:%M:%S%z")
+        end = end.astimezone(tz)
+
+        days_until = (start - now).days
+
+        title = event['summary']
+        if title.lower() == "lunch" or title.lower() == "brunch":
+            cards[days_until]["box1"]["small"] = title
+            cards[days_until]["box1"]["color"] = "#90EE90"
+            cards[days_until]["box1"]["large"] = f"{start.strftime('%I:%M %p')}"# - {end.strftime('%I:%M %p')}"
+        elif title.lower() == "dinner":
+            cards[days_until]["box2"]["small"] = "Dinner"
+            cards[days_until]["box2"]["color"] = "#90EE90"
+            cards[days_until]["box2"]["large"] = f"{start.strftime('%I:%M %p')}"# - {end.strftime('%I:%M %p')}"
+
+
+    if not events:
+        print('No upcoming events found.')
+
+    else:
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(f"{event['summary']} - {start}")
+
+
+    return render_template('index.html', cards=cards)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+ 
---
+++ readme.md
@@ -0,0 +1,4 @@
+`"We should get a meal and catch up!"` - Stanford students, with no intent to follow up
+
+# Let's get a meal!
+This is a minimal web page for easily scheduling meals. It syncs with a google calendar which has repeating lunch and dinner events. Any event in the next week which hasn't been renamed or removed is displayed on the web page.
